94735096e651cff776c0c2ae7b3e4f4796e6e37b
Felipe Figueiredo <felipe.adans89@gmail.com>
2020-01-13 23:36:08 -0200

    Exercicio 4.2

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -26,4 +26,31 @@ public class DescontoTest {
 		
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
+	
+	@Test
+	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
+		
+		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("20.5"));
+		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12"));				
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		
+		BigDecimal precoEsperado = new BigDecimal("22.75");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}	
+	
+	@Test
+	public void deveConcederDescontoDe50PorcentoParaIngressosDeEstudante() {
+		
+		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("20.5"));
+		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12"));				
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		
+		BigDecimal precoEsperado = new BigDecimal("16.25");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}	
+	
 }

ca22c95ed93e3ee0e47f5741ff9d6dde0f26c339
Felipe Figueiredo <felipe.adans89@gmail.com>
2020-01-15 22:50:04 -0300

    EX: 5.4, 5.5 e 6.2

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -9,18 +9,20 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 
 public class DescontoTest {
 
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-		
+		Lugar lugar = new Lugar("A", 1);
 		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12"));				
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 		
@@ -29,11 +31,11 @@ public class DescontoTest {
 	
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
-		
+		Lugar lugar = new Lugar("B", 1);
 		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12"));				
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.BANCO, lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		
@@ -42,15 +44,14 @@ public class DescontoTest {
 	
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressosDeEstudante() {
-		
+		Lugar lugar = new Lugar("C", 1);
 		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12"));				
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao,  TipoDeIngresso.ESTUDANTE, lugar);
 		
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
-	}	
-	
+	}
 }
--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -6,14 +6,20 @@ import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 
 public class GerenciadorDeSessaoTest {
 
@@ -88,5 +94,29 @@ public class GerenciadorDeSessaoTest {
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 		
 		Assert.assertEquals(somaDosPreosDaSalaEFilme, sessao.getPreco());
+	}
+	
+	@Test
+	public void garanteQueOLugarA1EstaOcupadoEOsLugaresA2EA3Disponiveis() {
+		
+		Lugar a1 = new Lugar("A",1);
+		Lugar a2 = new Lugar("A",2);
+		Lugar a3 = new Lugar("A",3);
+		
+		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120),"SCI-FI", new BigDecimal("12.0"));
+		
+		Sala eldorado7 = new Sala("Eldorado 7", new BigDecimal("8.5"));
+		
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, eldorado7);
+		
+		Ingresso ingresso = new Ingresso(sessao,  TipoDeIngresso.INTEIRO, a1);
+		
+		Set<Ingresso> ingressos = Stream.of(ingresso).collect(Collectors.toSet());
+		
+		sessao.setIngressos(ingressos);
+		
+		Assert.assertFalse(sessao.isDisponivel(a1));
+		Assert.assertTrue(sessao.isDisponivel(a2));
+		Assert.assertTrue(sessao.isDisponivel(a3));
 	}
 }
